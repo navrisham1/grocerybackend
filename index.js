a6f73752b7324c0c544499c15de62c275f198bae
“navrisham1” <navrisham1@gmail.com>
2025-03-24 20:54:28 -0400

    Adding IP address.

--- index.js
+++ index.js
@@ -68,6 +68,6 @@ app.post('/check-items', (req, res) => {
 });
 
 // Start the server
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
   console.log(`Server running on http://localhost:${port}`);
 });
